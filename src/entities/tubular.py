c99c19388f06fb48e548e50d20bf75cb6ed226fe
Daniel Duque Estrada Fernandes de Melo <danielduquemelo@puc-rio.br>
2026-03-19 16:55:48 -0300

    FEAT: Main entities for load, tubular and steel grade

---
+++ src/entities/tubular.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from numpy import pi
+from .steel_grade import SteelGrade
+
+@dataclass
+class TubularData:
+    """OCTG tubular
+    od: outer diameter in inches,
+    wt: wall thickness in inches,
+    ovality: ovality in percent,
+    eccentricity: eccentricity in percent
+    grade: steel grade object
+    ."""
+
+    od: float
+    wt: float
+    grade: SteelGrade
+    ovality: float = 0.0
+    eccentricity: float = 0.0
+
+
+    @property
+    def id(self) -> float:
+        return max(self.od - 2 * self.wt, 0.0)
+
+    @property
+    def od_to_wt_ratio(self) -> float:
+        """D/t ratio."""
+        return self.od / self.wt
+
+    @property
+    def wt_to_od_ratio(self) -> float:
+        """t/D ratio."""
+        return self.wt / self.od
+
+    @property
+    def area(self) -> float:
+        """Cross-sectional area of the tubular."""
+        return pi * self.od * self.wt
